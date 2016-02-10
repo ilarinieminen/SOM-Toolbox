802b980930c613f4dbf956990efa5287eae67462
Alejandro García Vallejo <vallejo.searcher@gmail.com>
2016-02-11 00:30:50 +0100

    Add backwards compatibility
    Backwards compatible numeric/struct colorbar handle fix

--- som/som_show.m
+++ som/som_show.m
@@ -490,9 +490,21 @@ for i=1:n,                         % main loop
   
   %%% Draw colorbars if they are turned on and the plane is umat or c-plane
 
-  h_colorbar(i,1)=colorbar(General.colorbardir);           % colorbars
-  if General.comp(i)< 0 || strcmp(General.colorbardir,'none'),
-    h_colorbar(i,1).Visible='off';
+  if General.comp(i)> -1 && ~strcmp(General.colorbardir,'none'),
+    h_colorbar(i,1)=colorbar(General.colorbardir);           % colorbars
+  else
+    %COMPATIBILITY HACK: This if...else... structure fixes a compatibility
+    %problem. In versions of MATLAB prior to 2014b colorbars returned a
+    %numeric handle. From 2014b onwards they return a structure, and they
+    %break backwards compatibility. At some point the numeric compatibility
+    %should be dropped, but for the time being this is a necessary small hack.
+    if verLessThan('matlab','8.4')
+        h_colorbar(i,1)=-1;
+    else
+        h_colorbar(i,1)=colorbar(General.colorbardir);           % colorbars
+        h_colorbar(i,1).Visible='off';
+    end
+    %END OF COMPATIBILITY HACK
     General.comp(i)=-1;
   end
 end         %% main loop ends
